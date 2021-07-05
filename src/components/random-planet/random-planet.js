ca4eaa9346ee96d88ad67f1156126d467574707c
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-07-05 15:27:37 +0300

    control commit

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -19,6 +19,7 @@ export default class RandomPlanet extends Component {
 	constructor() {
 		super();
 		this.updatePlanet();
+		setInterval(this.updatePlanet, 1500)
 	}
 
 	onPlanetLoaded = (planet) => {
@@ -35,8 +36,8 @@ export default class RandomPlanet extends Component {
 		})
 	}
 
-	updatePlanet() {
-		const id = 14;
+	updatePlanet = () => {
+		const id = Math.floor(Math.random() * 25 + 3);
 		this.swapiService
 		.getPlanet(id)
 		.then(this.onPlanetLoaded)
