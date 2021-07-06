172dafbaae83af14508e0d5faae5ea8727ef46c7
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-07-06 16:07:22 +0300

    componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,22 +2,11 @@ import {React, Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import PeoplePage from '../people-page';
 
 import './app.css';
 
 export default class App extends Component {
-
-	state = {
-		selectedPerson: null
-	}
-
-	onPersonSelected = (id) => {
-		this.setState({
-			selectedPerson: id
-		});
-	}
 
 	render(){
 		return (
@@ -25,14 +14,9 @@ export default class App extends Component {
 			<Header />
 			<RandomPlanet />
 	
-			<div className="row mb2">
-			<div className="col-md-6">
-				<ItemList onItemSelected = {this.onPersonSelected} />
-			</div>
-			<div className="col-md-6">
-				<PersonDetails personId = {this.state.selectedPerson} />
-			</div>
-			</div>
+			<PeoplePage />
+			<PeoplePage />
+			<PeoplePage />
 		</div>
 		);
 	}
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -22,6 +22,10 @@ export default class RandomPlanet extends Component {
 		//clearInterval(this.interval)
 	}
 
+	componentWillUnmount(){
+		clearInterval(this.interval)
+	}
+
 	onPlanetLoaded = (planet) => {
 		this.setState({
 		planet,
