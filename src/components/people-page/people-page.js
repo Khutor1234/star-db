844143931b8c29cb907c7b591956c0fcfcad7143
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-07-07 15:20:10 +0300

    children and ErrorBoundry

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,38 +3,20 @@ import React, { Component } from 'react';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
+import Row from '../row'
 
 import './people-page.css';
 import SwapiService from "../../services/swapi-service";
+import ErrorBoundry from '../error-boundry';
 
-const Row = ({left, right}) => {
-	return (
-		<div className="row mb2">
-			<div className="col-md-6">
-				{left}
-			</div>
-			<div className="col-md-6">
-				{right}
-			</div>
-		</div>
-		);
-}
 
 export default class PeoplePage extends Component {
 
 	swapiService = new SwapiService();
 
 	state = {
-		selectedPerson: 3,
-		hasError: false
+		selectedPerson: 3
 	};
-
-	componentDidCatch(error, info) {
-
-		this.setState({
-		hasError: true
-		});
-	}
 
 	onPersonSelected = (selectedPerson) => {
 		this.setState({ selectedPerson });
@@ -59,7 +41,9 @@ export default class PeoplePage extends Component {
 		)
 
 		return (
-			<Row left = {itemList} right ={personDetails}/>
+			<ErrorBoundry>
+				<Row left = {itemList} right ={personDetails}/>
+			</ErrorBoundry>
 		);
 	}
 }
