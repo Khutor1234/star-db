0e03249d728eb3490a45fb37d39251c21671a0e2
Alexandra Khutorna <khutornaalexandra@gmail.com>
2021-07-09 13:43:38 +0300

    hoc composition

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 
 import Header from '../header';
-import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 
-import Row from "../row/row";
-import ItemDetails, { Record } from "../item-details/item-details";
 import SwapiService from "../../services/swapi-service";
-
-import ItemList from '../item-list';
 
 import './app.css';
 
@@ -29,60 +24,13 @@ export default class App extends Component {
   };
 
   render() {
-
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
-
-    const { getPerson,
-            getStarship,
-            getPersonImage,
-            getStarshipImage,
-            getAllPeople,
-            getAllPlanets } = this.swapiService;
-
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage} >
-
-        <Record field="gender" label="Gender" />
-        <Record field="eyeColor" label="Eye Color" />
-
-      </ItemDetails>
-    );
-
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="Cost" />
-      </ItemDetails>
-    );
-
     return (
       <ErrorBoundry>
         <div className="stardb-app">
           <Header />
 
-          <ItemList
-            getData={getAllPeople}
-            onItemSelected={() => {}}>
-
-            { ({name}) => <span>{name}</span> }
-          </ItemList>
-
-          <ItemList
-            getData={getAllPlanets}
-            onItemSelected={() => {}}>
-
-            { ({name}) => <span>{name}</span> }
-          </ItemList>
+         <PersonList/>
+         <PersonDetails/>
 
         </div>
       </ErrorBoundry>
